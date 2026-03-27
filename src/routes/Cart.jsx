9fb05ef215db5035ef489c5661825c7ef98e6183
jswon <jswon7942@naver.com>
2026-03-27 19:59:37 +0900

    장바구니 소스 정리

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -1,4 +1,4 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 import { addCount, removeCount, removeItem } from "../Store";
 
 export default function Cart() {
@@ -24,39 +24,46 @@ export default function Cart() {
           </tr>
         </thead>
         <tbody>
-          {stateCart.length === 0 ?
-            (
-              <tr>
-                <td colspan='5'> 장바구니에 담긴 상품이 없습니다.</td>
+          {stateCart.length === 0 ? (
+            <tr>
+              <td colspan="5"> 장바구니에 담긴 상품이 없습니다.</td>
+            </tr>
+          ) : (
+            stateCart.map((item, index) => (
+              <tr key={index}>
+                <th scope="row">{index + 1}</th>
+                <td>{item.name}</td>
+                <td>{item.count}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      dispatch(addCount(item.id));
+                    }}
+                  >
+                    +
+                  </button>
+                  &nbsp;&nbsp;
+                  <button
+                    style={{ padding: "1px 7px 1px 7px" }}
+                    onClick={() => {
+                      dispatch(removeCount(item.id));
+                    }}
+                  >
+                    -
+                  </button>
+                </td>
+                <td>
+                  <button
+                    onClick={() => {
+                      dispatch(removeItem(item.id));
+                    }}
+                  >
+                    Del
+                  </button>
+                </td>
               </tr>
-            ) : (
-              stateCart.map((item, index) => (
-                <tr key={index}>
-                  <th scope="row">{index + 1}</th>
-                  <td>{item.name}</td>
-                  <td>{item.count}</td>
-                  <td>
-                    <button
-                      onClick={() => {
-                        dispatch(addCount(item.id))
-                      }}>+</button>
-                      &nbsp;&nbsp;
-                    <button
-                    style={{padding : '1px 7px 1px 7px'}}
-                      onClick={() => {
-                        dispatch(removeCount(item.id))
-                      }}>-</button>
-                  </td>
-                  <td>
-                    <button 
-                      onClick={() => {
-                        dispatch(removeItem(item.id));
-                      }}
-                    >Del</button>
-                  </td>
-                </tr>
-              )
-              ))}
+            ))
+          )}
         </tbody>
       </table>
     </div>
